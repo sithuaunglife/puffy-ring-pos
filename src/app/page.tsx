ffb517b69cf609515f6bb88a3d9be38068e9d582
Sithu Aung <208134364+sithuaunglife@users.noreply.github.com>
2026-05-07 10:39:39 +0630

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { ComponentExample } from "@/features/dashboard/components/component-example";
+
+export default function Page() {
+  return <ComponentExample />;
+}
---
+++ src/features/auth/components/LoginForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const LoginForm = (props: Props) => {
+  return (
+    <div>LoginForm</div>
+  )
+}
+
+export default LoginForm
